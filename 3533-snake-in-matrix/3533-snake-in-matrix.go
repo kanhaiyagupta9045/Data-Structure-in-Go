81fb605af10a9bbc434ba6ca76e0cf161755a46a
kanhaiyagupta9045 <120053612+kanhaiyagupta9045@users.noreply.github.com>
2024-08-11 14:45:42 +0530

    Time: 0 ms (100%), Space: 5.3 MB (100%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.go
@@ -0,0 +1,29 @@
+func finalPositionOfSnake(n int, commands []string) int {
+	i := 0
+	j := 0
+	matrix := make([][]int, n)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
+    cnt :=0
+    for a :=0;a<len(matrix);a++{
+        for b:=0;b<n;b++{
+            matrix[a][b] = cnt
+            cnt+=1
+        }
+    }
+
+	for k := 0; k < len(commands); k++ {
+		if commands[k] == "RIGHT" {
+			j += 1
+		} else if commands[k] == "UP" {
+			i = i - 1
+		} else if commands[k] == "DOWN" {
+			i = i + 1
+		} else {
+			j = j - 1
+		}
+	}
+    return matrix[i][j]
+
+}
