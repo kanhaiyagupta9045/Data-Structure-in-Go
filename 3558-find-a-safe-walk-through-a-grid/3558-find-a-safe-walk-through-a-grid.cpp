60df911c41f1b0071b148e60b8f1721f5b1ac3b8
kanhaiyagupta9045 <120053612+kanhaiyagupta9045@users.noreply.github.com>
2024-09-17 01:22:20 +0530

    Time: 44 ms (67.2%), Space: 30.8 MB (79.06%) - LeetHub

---
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int dx[4] = {-1, 1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
+        int m = grid.size();
+        int n = grid[0].size();
+
+        priority_queue<pair<int, pair<int, int>>> q;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        health = health - (grid[0][0] == 1 ? 1 : 0);
+        q.push({health, {0, 0}});
+        set<int> s;
+        while (!q.empty()) {
+            auto it = q.top();
+            q.pop();
+            int row = it.second.first;
+            int col = it.second.second;
+            int current_health = it.first;
+            if (row == m - 1 && col == n - 1) {
+                s.insert(current_health);
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int newRow = row + dy[i];
+                int newCol = col + dx[i];
+
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n &&!vis[newRow][newCol]) {
+                    if (grid[newRow][newCol] == 1) {
+                        q.push({current_health - 1, {newRow, newCol}});
+                    } else {
+                        q.push({current_health, {newRow, newCol}});
+                    }
+                    vis[newRow][newCol] = 1;
+                }
+            }
+        }
+        for(auto it : s){
+            if(it>=1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
