75edec43416263f0705e0f4e76568edf240ab907
kanhaiyagupta9045 <120053612+kanhaiyagupta9045@users.noreply.github.com>
2024-09-23 17:04:56 +0530

    Time: 6 ms (20.71%), Space: 10.5 MB (67.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -7,7 +7,6 @@ public:
         if(i<0 || j<0){
             return 0;
         }
-
         int a = 0;
         int b = 0;
         if(dp[i][j]!=-1){
@@ -28,7 +27,30 @@ public:
         if (grid[0][0] == 1 || grid[m - 1][n - 1] == 1) {
             return 0;
         }
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,m,n,grid,dp);
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // return f(m-1,n-1,m,n,grid,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0] = 1;
+        for(int  i = 1;i<n;i++){
+            if(grid[0][i]==0){
+                dp[0][i]=dp[0][i-1];
+            }
+        }
+        for(int  i = 1;i<m;i++){
+            if(grid[i][0]==0){
+                dp[i][0]=dp[i-1][0];
+            }
+        }
+
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                if(grid[i][j]==0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
