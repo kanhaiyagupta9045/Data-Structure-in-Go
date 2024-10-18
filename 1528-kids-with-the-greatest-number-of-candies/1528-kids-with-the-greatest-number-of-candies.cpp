30461d6612c78884aed73c1803cfb3c74523a7e4
kanhaiyagupta9045 <120053612+kanhaiyagupta9045@users.noreply.github.com>
2024-10-19 01:22:01 +0530

    Time: 0 ms (100%), Space: 11.3 MB (21.97%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi = *max_element(candies.begin(),candies.end());
+
+        vector<bool> ans;
+
+        for(int i = 0;i<candies.size();i++){
+            if(candies[i]==maxi){
+                ans.push_back(true);
+            }else{
+                int diff = maxi - candies[i];
+
+                if(extraCandies>=diff){
+                    ans.push_back(true);
+                }else{
+                    ans.push_back(false);
+                }
+            }
+        }
+        return ans;
+    }
+};
